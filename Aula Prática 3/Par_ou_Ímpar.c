c7dffd2586b01503fa06696d82b49d30f325a0c5
João Pedro Duarte Martins Xavier <joaopedrodmx@gmail.com>
2025-09-28 12:38:09 -0300

    Update Par_ou_Ímpar.c

--- Aula Prática 3/Par_ou_Ímpar.c
+++ Aula Prática 3/Par_ou_Ímpar.c
@@ -11,4 +11,4 @@ int parOuImpar(unsigned int number){ // Função e seus parâmetros.
 /* Como funciona a função:
 	1 - Cada um dos bits de number é negado. Então, se for par, o bit menos significativo (LSB) será, a partir de agora, 1 e, se for ímpar, o LSB será 0.
 	2 - Em seguida 'number & 1' zera todos os demais bits e mantém somente o LSB inalterado. Resultando em 1 se 'number' era par e 0 se 'number' era ímpar.
-	Assim, o resultado final é armazenado em 'number' e é retornado pela função.
+	Assim, o resultado final é armazenado em 'number' e é retornado pela função. */
